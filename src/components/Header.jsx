8aac1a12db0694ee6647e1c9fd0855eb9b829090
matiasnnr <matias.nnr@hotmail.com>
2020-09-29 15:51:33 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../assets/styles/components/Header.scss';
+
+const Header = () => (
+    <header className="header">
+    <img className="header__img" src="../assets/logo-video.png" alt="Matías Video"/>
+    <div className="header__menu">
+      <div className="header__menu--profile">
+        <img src="../assets/user-icon.png" alt=""/>
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href="/">Cuenta</a></li>
+        <li><a href="/">Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </header>
+)
+
+export default Header;
