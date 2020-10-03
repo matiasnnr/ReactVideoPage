59425f3bd23457eae4c54f300fce0f263d84f0c7
matiasnnr <matias.nnr@hotmail.com>
2020-10-03 12:03:14 -0300

    Updates

---
+++ precache-manifest.fb6a829599a7eba7323fec987ea04fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2877c4089b52620c67c54f071a776469",
+    "url": "/react-video-page/index.html"
+  },
+  {
+    "revision": "210823b2323f265e9b51",
+    "url": "/react-video-page/static/css/main.90279d32.chunk.css"
+  },
+  {
+    "revision": "f35bb63eed4090fe47fe",
+    "url": "/react-video-page/static/js/2.77a4995a.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/react-video-page/static/js/2.77a4995a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210823b2323f265e9b51",
+    "url": "/react-video-page/static/js/main.f269349b.chunk.js"
+  },
+  {
+    "revision": "be405255f197720d9c5e",
+    "url": "/react-video-page/static/js/runtime-main.447a1f5d.js"
+  },
+  {
+    "revision": "9b8dcdfd71818d342a76f2c13beed26b",
+    "url": "/react-video-page/static/media/logo-video.9b8dcdfd.png"
+  }
+]);
